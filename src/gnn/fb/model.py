2570bcd8d6a1ca92da005a290bba3abaa427b352
An Hoang Nguyen <nguyenan.sadek@outlook.com>
2025-09-21 08:54:48 +0700

    Them dropout

--- src/gnn/fb/model.py
+++ src/gnn/fb/model.py
@@ -5,13 +5,15 @@ from torch_geometric.nn import GCNConv
 
 
 class GCNEncoder(nn.Module):
-    def __init__(self, in_channels, out_channels):
+    def __init__(self, in_channels, out_channels, dropout=0.5):
         super().__init__()
         self.conv1 = GCNConv(in_channels, 128)
         self.conv2 = GCNConv(128, out_channels)
+        self.dropout = dropout
 
     def forward(self, x, edge_index):
         x = F.relu(self.conv1(x, edge_index))
+        x = F.dropout(x, p=self.dropout, training=self.training)
         return self.conv2(x, edge_index)
 
 
